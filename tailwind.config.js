8c68a9412ab7d52d42377359d8e7353955d690c0
abyanmusyaffa <abyanaufaalifmusyaffa@gmail.com>
2024-10-09 11:18:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}",  "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    addDynamicIconSelectors(),
+    require('flowbite/plugin'),
+  ],
+}
